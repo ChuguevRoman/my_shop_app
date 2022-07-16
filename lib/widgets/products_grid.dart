96264a667b6a768f44c4997c0c2a1ad7b84cce80
ChuguevRoman <5tiger5xman@gmail.com>
2022-07-16 19:10:09 +0300

    Moved the grid to a separate widget. Set up a listener.

---
+++ lib/widgets/products_grid.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:my_shop_app/providers/products.dart';
+import 'package:provider/provider.dart';
+
+import '../widgets/product_item.dart';
+
+class ProductsGrid extends StatelessWidget {
+  const ProductsGrid({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final products = context.watch<Products>().items;
+    return GridView.builder(
+      padding: const EdgeInsets.all(8),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+      itemCount: products.length,
+      itemBuilder: (ctx, index) => ProductItem(
+        products[index].id,
+        products[index].title,
+        products[index].imageUrl,
+      ),
+    );
+  }
+}
